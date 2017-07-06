9532d10f4a6c1aa7c32ef13620c3ddef84c548b7
“Lemon <xyydying@163.com>
2017-07-06 10:39:07 +0800

    改版更新

--- js/common.js
+++ js/common.js
@@ -12,23 +12,6 @@ var commonlist = function() {
 			}
 		});
 	});
-	//移动端一级分类显示（动车、客车、城轨）
-	$(".m_nav_content_ul1").hide();
-	$(".about_m_List").children("a").click(function() {
-		$(".m_nav_content_ul1").slideToggle(300);
-	});
-	//移动端二级分类展示（配件、设备、维保）
-	$(".m_nav_content_ul2").hide();
-	$(".m_nav_content_ul1").children("li").children("h4").click(function() {
-		$(this).parent("li").children(".m_nav_content_ul2").slideToggle(300);
-		$(this).parent("li").siblings("li").children(".m_nav_content_ul2").hide(300);
-	});
-	//移动端三级分类展示（温控系统等）
-	$(".m_nav_content_ul3").hide();
-	$(".m_nav_content_ul2").children("li").children("h5").click(function() {
-		$(this).parent("li").children(".m_nav_content_ul3").slideToggle(300);
-		$(this).parent("li").siblings("li").children(".m_nav_content_ul3").hide(300);
-	});
 	//标头样式
 	$('#list_content').on('mouseover', function() {
 		$(".xd_pro_List").show().on('mouseleave', function() {
@@ -81,82 +64,6 @@ var commonlist = function() {
 		}
 	});
 
-	//懒人仓网站共部分
-	//导航pc下拉
-	var pcbojbox = $(".pc_nav").children("ul").children("li.example_pc_btn");
-	var pcbojbox_ullist = $(".pc_nav").children("ul").children("li").children('ul');
-	pcbojbox.on("mouseenter", function() {
-		pcbojbox.children('ul').show();
-	})
-	pcbojbox.siblings('li').on("mouseenter", function() {
-		pcbojbox.children('ul').hide();
-	})
-	pcbojbox_ullist.on("mouseleave", function() {
-		pcbojbox_ullist.hide();
-	})
-
-	//	pcbojbox.children('ul').on("mouseleave",pcbojbox,function(){
-	//		$(this).hide();
-	//	})
-	//	pcbojbox.children('ul').on("mouseleave",function(){
-	//		$(this).hide();
-	//	})
-	//	
-	//tab切换mobile
-	//获取m端配件按钮
-	var m_tab_btn_peij = $(".m_tab_btn_peij").children('li');
-	//获取m端设备按钮
-	var m_tab_btn_sheb = $(".m_tab_btn_sheb").children('li');
-	//获取m端维保按钮
-	var m_tab_btn_weib = $(".m_tab_btn_weib").children('li');
-	var mobile_tab_btn_arr = [m_tab_btn_peij, m_tab_btn_sheb, m_tab_btn_weib];
-
-	m_tab_btn_peij.on('click', function() {
-		$('.m_nav_content').slideUp(300);
-	});
-	m_tab_btn_sheb.on('click', function() {
-		$('.m_nav_content').slideUp(300);
-	});
-	m_tab_btn_weib.on('click', function() {
-		$('.m_nav_content').slideUp(300);
-	});
-
-	$(".exanple_m_btn").children('a').on("click", function() {
-		$(this).siblings('.example_m').slideToggle(300);
-	})
-	$(".example_m").on("click", function() {
-		$(".m_nav_content").slideUp(300);
-	})
-
-	//获取pc端按钮
-	var btnLoadImg = $('.xd_pro_List').find('ul').children('li');
-	//获取m端按钮
-	var m_btnLoadImg = $('.m_nav_content_ul1').find('ul').children('li');
-	//右侧分享鼠标移动事件
-	$("#chat a").hover(function() {
-		if ($(this).prop("className") == "youhui") {
-			$(this).children("img.hides").show();
-		} else {
-			$(this).children("img.hides").show();
-			$(this).children("img.shows").hide();
-			$(this).children("img.hides").animate({
-				marginRight: '0px'
-			}, 'slow');
-		}
-	}, function() {
-		if ($(this).prop("className") == "youhui") {
-			$(this).children("img.hides").hide('slow');
-		} else {
-			$(this).children("img.hides").animate({
-				marginRight: '-143px'
-			}, 'slow', function() {
-				$(this).hide();
-				$(this).next("img.shows").show();
-			});
-		}
-	});
-
-	//懒人仓网站公共部分结束
 };
 
 //获取url中的某个参数
@@ -173,11 +80,6 @@ var tab_location = function(num, index, contentObjsArr) {
 		contentObjs.hide();
 		if (num == contentnum) {
 			$(contentObjs[index]).show();
-			//$.each(contentObjs, function (contentindex, content) {
-			//    if (contentindex == index) {
-			//        $(content).show();
-			//    }
-			//})
 		}
 	})
 };
@@ -189,8 +91,6 @@ var setprobucturl = function(btnarr, btnLoadImg, url) {
 		$.each(tabbtnObjs, function(index, item) {
 			$(item).on('click', function() {
 				urlJSON = url + '?&num=' + num + '&index=' + index;
-				// window.location.href = urlJSON;
-				console.log(urlJSON);
 			})
 		})
 	});
@@ -206,58 +106,55 @@ var setprobucturl = function(btnarr, btnLoadImg, url) {
 
 //点击tab加载相应的图片
 var tabLoadImg = function(index) {
-		// console.log($(this));
-		// console.log($('.xd_pr_content').children('div'));
-		var imgbox = $('.xd_pr_content').children('div');
+		var imgbox = $('.cang_service_content').children('.cang_service_conpea');
 		$(imgbox[index]).find('img').each(function(i, item) {
 			var srcstr = $(item).attr('data-src');
 			$(item).attr('src', srcstr);
-			console.log(item);
 		})
 	}
 	//js实现锚链接定位效果
 	/**
 	 * 
+	 * @param {Object} targetObj 目标对象
 	 * @param {Object} clickObj 为对象时是点击的按钮，与targetObj一一对应，为object增加事件，其余为直接滑动
 	 * @param {string}{number}clickObj	为string或者number时其对应targetObj中的第几个元素
-	 * @param {Object} targetObj 目标对象
-	 * @param {Object} changeTop 对scrollTop微调，小与三个参数为默认值
+	 * @param {number} changeTop 对scrollTop微调，小与三个参数为默认值
 	 * 参数为一个时clickObj为默认值0 此时只对应targetObj第一个
-	 * 参数为两个时changeTop为默认值
+	 * 参数为两个时changeTop为默认值105
 	 */
-var jumpTo = function(targetObj,clickObj,changeTop) {
+var jumpTo = function(targetObj, clickObj, changeTop) {
 
-		changeTop ? changeTop : 105;
-		if (arguments.length > 1) {
-			if (typeof clickObj == 'object') {
-				$.each(clickObj, function(index, item) {
-					$(item).on('click', function() {
-						var scrolltop = $(targetObj[index]).offset().top - 105
-						$('html body').animate({
-							scrollTop: scrolltop
-						}, {
-							duration: 500,
-							easing: 'swing'
-						});
-					})
+	changeTop ? changeTop : 105;
+	if (arguments.length > 1) {
+		if (typeof clickObj == 'object') {
+			$.each(clickObj, function(index, item) {
+				$(item).on('click', function() {
+					var scrolltop = $(targetObj[index]).offset().top - 105
+					$('html body').animate({
+						scrollTop: scrolltop
+					}, {
+						duration: 500,
+						easing: 'swing'
+					});
 				})
-			} else {
-				var scrolltop = $(targetObj[clickObj]).offset().top - 105;
-				$('html body').animate({
-					scrollTop: scrolltop
-				}, {
-					duration: 500,
-					easing: 'swing'
-				});
-			}
+			})
 		} else {
-			var scrolltop = targetObj.offset().top - 105;
+			var scrolltop = $(targetObj[clickObj]).offset().top - 105;
 			$('html body').animate({
-					scrollTop: scrolltop
-				}, {
-					duration: 500,
-					easing: 'swing'
-				})
-			}
-
+				scrollTop: scrolltop
+			}, {
+				duration: 500,
+				easing: 'swing'
+			});
 		}
+	} else {
+		var scrolltop = targetObj.offset().top - 105;
+		$('html body').animate({
+			scrollTop: scrolltop
+		}, {
+			duration: 500,
+			easing: 'swing'
+		})
+	}
+
+}
--- js/index.js
+++ js/index.js
@@ -6,6 +6,12 @@ window.onload = function() {
 		//设置自动播放秒
 		interval: 100000000000000,
 	});
+	//轮播图懒加载
+	$('#carousel-example-generic').bind('slid.bs.carousel', function() {
+		lazyContainer(this);
+	});
+
+	lazyContainer('#carousel-example-generic');
 	//移动端手势
 	var startX = 0;
 	var moveX = 0;
@@ -40,26 +46,56 @@ window.onload = function() {
 			var initiaLng = $(this).attr("data-initiaLng");
 			var initiaLat = $(this).attr("data-initiaLat");
 			addcontent(cityShow);
-			maps(initiaLng, initiaLat);
+			maps(stores, cityShow);
 		})
 		//信息初始化
-	addcontent('chaoyang');
-	maps(116.447156, 39.979679);
-	if(getUrlParam('lsfd')){
+	addcontent();
+	maps(stores);
+	if (getUrlParam('lsfd')) {
 		jumpTo($('.cang_dian'));
+	} else if (getUrlParam('ccfw')) {
+		jumpTo($('.cang_introduce'));
 	}
 	//点击定位到地图
-	jumpTo($('.cang_dian'),$('.jumppc_self_lsfd'));
-	jumpTo($('.cang_dian'),$('.jumpmobile_self_lsfd'));
-	
+	jumpTo($('.cang_dian'), $('.jumppc_self_lsfd'));
+	jumpTo($('.cang_dian'), $('.jumpmobile_self_lsfd'));
+	jumpTo($('.cang_introduce'), $('.exanple_m_btn'));
+	jumpTo($('.cang_introduce'), $('.example_pc_btn'));
+
+	//仓型服务tab切换
+	$(".cang_service_title").find("li").on("click", function() {
+		$(this).addClass("cang_ser_titon").siblings("li").removeClass("cang_ser_titon");
+		var index = $(this).index();
+		$(".cang_service_content").find(".cang_service_conpea").eq(index).show().siblings().hide();
+		var datasrcObj = $(".cang_service_content").find(".cang_service_conpea").eq(index).find('img');
+		console.log(datasrcObj);
+		$.each(datasrcObj, function(index, item) {
+			console.log(item);
+			var datasrc = $(item).attr('data-src');
+			if (datasrc) {
+				$(item).attr("src", datasrc);
+			}
+		})
+	})
+
 };
+//轮播图懒加载
+function lazyContainer(searchNode) {
+	$(searchNode).find('.active').find('img.lazy').each(function() {
+		var imgSrc = $(this).attr('data-src');
+		if (imgSrc) {
+			$(this).attr('src', imgSrc);
+			$(this).attr('data-src', '');
+		}
+	});
+}
 
 function addcontent(cityShow) {
+	cityHtml = '';
 	for (var key in stores) {
-		if (key == cityShow) {
-			cityHtml = '';
+		if (!cityShow) {
 			$.each(stores[key], function(index, item) {
-				cityHtml += '<div class="result-item" onclick = maps(' + item.storeCoordinateLng + ',' + item.storeCoordinateLat + ')>';
+				cityHtml += '<div class="result-item" onclick = maps(' + item.storeCoordinateLng + ',' + item.storeCoordinateLat + ',1)>';
 				cityHtml += '<div class="txt-wrap">';
 				cityHtml += '<h3>' + item.storeName + '</h3>';
 				cityHtml += '<div class="txt-desc">';
@@ -68,30 +104,73 @@ function addcontent(cityShow) {
 				cityHtml += '<p class="metro-station">' + item.storeMetro + '</p>';
 				cityHtml += '</div></div></div>';
 			})
-			$('#r-result-beijing').html(cityHtml);
+		} else if (key == cityShow) {
+			$.each(stores[key], function(index, item) {
+				cityHtml += '<div class="result-item" onclick = maps(' + item.storeCoordinateLng + ',' + item.storeCoordinateLat + ',1)>';
+				cityHtml += '<div class="txt-wrap">';
+				cityHtml += '<h3>' + item.storeName + '</h3>';
+				cityHtml += '<div class="txt-desc">';
+				cityHtml += '<p class="address">' + item.storeAddress + '</p>';
+				cityHtml += '<p class="bus-stop">' + item.storeBusstop + '</p>';
+				cityHtml += '<p class="metro-station">' + item.storeMetro + '</p>';
+				cityHtml += '</div></div></div>';
+			})
 		}
 	}
+
+	$('#r-result-beijing').html(cityHtml);
 }
-
-function maps(lng, lat, mess) {
-	mess ? mess = mess : mess = '';
+/**
+ * 当参数为三个时设置地图的一个标记
+ * @param {number} param1 代表经度lng
+ * @param {number} param2 代表维度lat
+ * @param {string} param3 代表对应的标注mess
+ * 当参数为两个时显示数据中对应key内的标记
+ * @param {object} param1 json数据
+ * @param {string} param2 对应key
+ * storeCoordinateLng 提供lng
+ * storeCoordinateLat 提供lat
+ * mess暂未定义
+ * 当参数为一个时显示数据中所有标记
+ * @param {object} param1 json数据
+ * storeCoordinateLng 提供lng
+ * storeCoordinateLat 提供lat
+ * mess暂未定义
+ */
+function maps(param1, param2, param3) {
 	var map = new BMap.Map("dituContent"); // 创建Map实例
-	var point = new BMap.Point(lng, lat); // 创建点坐标  
-	map.centerAndZoom(point, 15); // 初始化地图,用城市名设置地图中心点
-	map.enableScrollWheelZoom(true);
-	var marker = new BMap.Marker(point); // 创建标注
-	map.addOverlay(marker); // 将标注添加到地图中
-	marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
-	var label = new BMap.Label(mess, {
-		offset: new BMap.Size(-100, -50)
-	});
-	marker.setLabel(label);
+	if (arguments.length == 3) {
+		var lng = param1;
+		var lat = param2;
+		var mess = param3;
+		mess ? mess = mess : mess = '';
+		var point = new BMap.Point(lng, lat); // 创建点坐标  
+		map.centerAndZoom(point, 15); // 初始化地图,用城市名设置地图中心点
+		map.enableScrollWheelZoom(true);
+		var marker = new BMap.Marker(point); // 创建标注
+		map.addOverlay(marker); // 将标注添加到地图中
+	} else {
+		var stores = param1;
+		var cityShow = param2;
+		for (var key in stores) {
+			if (!cityShow) {
+				$.each(stores[key], function(index, item) {
+					var point = new BMap.Point(item.storeCoordinateLng, item.storeCoordinateLat); // 创建点坐标  
+					map.centerAndZoom('天安门', 11); // 初始化地图,用城市名设置地图中心点
+					map.enableScrollWheelZoom(true);
+					var marker = new BMap.Marker(point); // 创建标注
+					map.addOverlay(marker); // 将标注添加到地图中
+				})
+			} else if (key == cityShow) {
+				$.each(stores[key], function(index, item) {
+					var point = new BMap.Point(item.storeCoordinateLng, item.storeCoordinateLat); // 创建点坐标  
+					map.centerAndZoom('天安门', 11); // 初始化地图,用城市名设置地图中心点
+					map.enableScrollWheelZoom(true);
+					var marker = new BMap.Marker(point); // 创建标注
+					map.addOverlay(marker); // 将标注添加到地图中
+				})
+			}
+		}
 
-	label.setStyle({
-		color: "#ca1b2d",
-		fontSize: "16px",
-		backgroundColor: "1",
-		border: "0",
-		fontWeight: "bold"
-	});
+	}
 }
